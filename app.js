812876ffc9b6c30e2295333aa511a753bab6eec2
HugoBDev <hugobrd.pro@gmail.com>
2023-11-30 22:25:19 +0100

    add bottomGradient

--- app.js
+++ app.js
@@ -36,43 +36,43 @@ const questionsArray = [
         'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/909JHryBRMXJeVswBUDlYWK7cQr.jpg'
     ),
 
-    // //QUESTION 3
-    // new Question(
-    //     'Quel almùklmùcteur joue dans clmùlùmklmùk film ?'
-    //     , [
-    //         'Morgan Freeman'
-    //         , 'Hugues Jackman'
-    //         , 'Matt Demon'
-    //     ],
-    //     2,
-    //     'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/boAUuJBeID7VNp4L7LNMQs8mfQS.jpg'
-    // ),
+    //QUESTION 3
+    new Question(
+        'Quel almùklmùcteur joue dans clmùlùmklmùk film ?'
+        , [
+            'Morgan Freeman'
+            , 'Hugues Jackman'
+            , 'Matt Demon'
+        ],
+        2,
+        'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/boAUuJBeID7VNp4L7LNMQs8mfQS.jpg'
+    ),
 
 
-    // //QUESTION 4
-    // new Question(
-    //     'Quel acteur jklmùklmùklmùoue dans ce film ?'
-    //     , [
-    //         'Morgan Freeman'
-    //         , 'Hugues Jackman'
-    //         , 'Matt Demon'
-    //     ],
-    //     2,
-    //     'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/boAUuJBeID7VNp4L7LNMQs8mfQS.jpg'
-    // ),
+    //QUESTION 4
+    new Question(
+        'Quel acteur jklmùklmùklmùoue dans ce film ?'
+        , [
+            'Morgan Freeman'
+            , 'Hugues Jackman'
+            , 'Matt Demon'
+        ],
+        2,
+        'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/boAUuJBeID7VNp4L7LNMQs8mfQS.jpg'
+    ),
 
-    // //QUESTION 5
-    // new Question(
-    //     'Quel acteur ne joue pas dans ce film de qualité ?',
-    //     [
-    //         'Gérard Jugnot'
-    //         , 'Thierry Lhermitte'
-    //         , 'Gérard Depardieu',
-    //         'Alain Chabat'
-    //     ],
-    //     2,
-    //     'https://fr.web.img5.acsta.net/pictures/23/06/21/12/06/4953335.jpg'
-    // ),
+    //QUESTION 5
+    new Question(
+        'Quel acteur ne joue pas dans ce film de qualité ?',
+        [
+            'Gérard Jugnot'
+            , 'Thierry Lhermitte'
+            , 'Gérard Depardieu',
+            'Alain Chabat'
+        ],
+        2,
+        'https://fr.web.img5.acsta.net/pictures/23/06/21/12/06/4953335.jpg'
+    ),
 ];
 
 let score = 0;
@@ -92,37 +92,40 @@ function createQuestionBlock(q, index) {
     posterContainer.classList.add('poster-container');
     const posterImg = document.createElement('img');
     posterImg.src = question.poster;
+    const bottomGradient = document.createElement('div');
+    bottomGradient.classList.add('bottom-gradient');
 
-    const titleQuestion = document.createElement('p')
+    const titleQuestion = document.createElement('p');
     // la je lui dis que ce que je veux à la place de mon texte c'est ce que j'ai dans mon objet Question
     titleQuestion.textContent = question.statement;
 
     const formAnswer = document.createElement('form');
-    formAnswer.id = "question" + index
+    formAnswer.id = "question" + index;
 
-    //! la c'est le bordel, accroche toi à ton slip// 
+    // la c'est le bordel, accroche toi à ton slip// 
     // On boucle dans mon tableau de liste d'acteur(answerList), à chaque fois que je bouvcle, je creer la partie radio+label+actor
     for (let i = 0; i < question.answerList.length; i++) {
 
         const answerWrapper = document.createElement('div');
         answerWrapper.classList.add('answer-wrapper');
-        const label = document.createElement('label')
+        const label = document.createElement('label');
         label.htmlFor = index;
-        label.textContent = question.answerList[i]
+        label.textContent = question.answerList[i];
 
-        const input = document.createElement('input')
-        input.type = "radio"
-        input.name = "actor"
+        const input = document.createElement('input');
+        input.type = "radio";
+        input.name = "actor";
         input.id = index;
-        input.value = i
+        input.value = i;
 
         // la je range mon input et mon label dans mon answer-wrapper// 
-        answerWrapper.appendChild(label)
-        answerWrapper.appendChild(input)
-        formAnswer.appendChild(answerWrapper)
+        answerWrapper.appendChild(label);
+        answerWrapper.appendChild(input);
+        formAnswer.appendChild(answerWrapper);
     }
     // en gros on range les boites dans d'autres boites // 
-    posterContainer.appendChild(posterImg)
+    posterContainer.appendChild(posterImg);
+    posterContainer.appendChild(bottomGradient);
     divQuestion.appendChild(posterContainer)
     divQuestion.appendChild(titleQuestion)
     divQuestion.appendChild(formAnswer)
@@ -168,13 +171,12 @@ function getScore(questionIndex) {
 
 
 /**
- * 
+ * La fonction qui affiche le score final
  */
 function displayScores() {
     questionsArray.forEach((question, index) => {
         getScore(index);
     })
-
 
     const scoreDiv = document.createElement('p');
     scoreDiv.textContent = `${score} / ${questionsArray.length}`;
@@ -182,4 +184,6 @@ function displayScores() {
     document.body.appendChild(scoreDiv);
 }
 
+
+// START 🚀
 generateQuestions(questionsArray);
