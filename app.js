ababa406c4a175cb1d27cdf89d64b2f440da1fbc
HugoBDev <hugobrd.pro@gmail.com>
2023-12-01 20:32:53 +0100

    fix submit btn position.

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 
 
-import config from './config.js';
 import Tmdb from './tmdb.js';
 import Question from './models/question.model.js';
 import Movie from './models/movie.model.js';
@@ -8,79 +7,10 @@ const tmdb = new Tmdb;
 
 let score = 0;
 
-function createQuestionBlock(q, index) {
-
-    // ca c'est la question que je met en parametre
-    const question = new Question(q.statement, q.answerList, q.correctAnswer, q.poster);
-
-    // la je creer la div principale de ma question
-    const divQuestion = document.createElement('div');
-    // la je lui ajoute ses classes
-    divQuestion.classList.add('block', 'question');
-
-    // la je lui ajoute ses classes
-    const posterContainer = document.createElement('div');
-    posterContainer.classList.add('poster-container');
-    const posterImg = document.createElement('img');
-    posterImg.src = question.poster;
-    const bottomGradient = document.createElement('div');
-    bottomGradient.classList.add('bottom-gradient');
-
-    const titleQuestion = document.createElement('p');
-    // la je lui dis que ce que je veux à la place de mon texte c'est ce que j'ai dans mon objet Question
-    titleQuestion.textContent = question.statement;
-
-    const questionsTest = document.createElement('div');
-    questionsTest.classList.add('questions-container');
-
-    // la c'est le bordel, accroche toi à ton slip// 
-    // On boucle dans mon tableau de liste d'acteur(answerList), à chaque fois que je bouvcle, je creer la partie radio+label+actor
-    for (let i = 0; i < question.answerList.length; i++) {
-
-        const labelRef = document.createElement('label');
-        const inputRef = document.createElement('input');
-        inputRef.type = 'radio';
-        inputRef.name = 'answer'+ index;
-
-
-        const spanRef = document.createElement('span');
-        spanRef.textContent = question.answerList[i]; 
-
-        labelRef.appendChild(inputRef);
-        labelRef.appendChild(spanRef);
-
-
-
-
-        questionsTest.appendChild(labelRef)
-
-        
-    }
-    // en gros on range les boites dans d'autres boites // 
-    posterContainer.appendChild(posterImg);
-    posterContainer.appendChild(bottomGradient);
-    divQuestion.appendChild(posterContainer)
-    divQuestion.appendChild(titleQuestion)
-    divQuestion.appendChild(questionsTest)
-    document.body.appendChild(divQuestion);
-    
-   
-}
-
-
 /**
- * Une fonction qui permet de générer les blocs de questioon 
- * à affichier.
  * 
- * @param {Question[]} questionList 
+ * @param {*} questionIndex 
  */
-function generateQuestions(questionList) {
-    questionList.forEach((ques, i) => {
-        createQuestionBlock(ques, i)
-    });
-}
-
-
 function getScore(questionIndex) {
     var formulaire = document.getElementById("question" + questionIndex);
     let answer = formulaire.querySelector('input[name="actor"]:checked');
@@ -120,14 +50,8 @@ function displayScores() {
     document.body.appendChild(scoreDiv);
 }
 
-function generateQuestionsByMovie(movie){
-
-}
-
 
 // START 🚀
-// generateQuestions(questionsArray);
-
 
 tmdb.discoverMovies(2).then(data => {
     let i = 0;
@@ -141,12 +65,11 @@ tmdb.discoverMovies(2).then(data => {
                     credit.cast[1].name,
                     credit.cast[2].name,
                     credit.cast[3].name,
-            ]
-
+            ];
+        
             const imgUrl = `https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}`; 
-            const question = new Question('Lequel de ces acteurs n \'as pas joué dans ' + data.results[i].title + ' ?', answerList, 0,  imgUrl)
-
-            createQuestionBlock(question, i)
+            const question = new Question('Lequel de ces acteurs n \'as pas joué dans ' + data.results[i].title + ' ?', answerList, 0,  imgUrl);
+            question.createQuestionBlock(i);
         })
 
 
@@ -157,7 +80,6 @@ tmdb.discoverMovies(2).then(data => {
         }
         i += 1;
     }, 500)
-
 
 }).catch(e)(
     console.error(e)
--- models/question.model.js
+++ models/question.model.js
@@ -1,8 +1,67 @@
 export default class Question {
+
+    statement;
+    answerList
+    correctAnswer;
+    poster;
+
     constructor(statement, answerList, correctAnswer, poster) {
         this.statement = statement;
         this.answerList = answerList;
         this.correctAnswer = correctAnswer;
         this.poster = poster;
     }
+
+
+    createQuestionBlock(index) {
+
+        // ca c'est la question que je met en parametre
+
+        // la je creer la div principale de ma question
+        const divQuestion = document.createElement('div');
+        // la je lui ajoute ses classes
+        divQuestion.classList.add('block', 'question');
+
+        // la je lui ajoute ses classes
+        const posterContainer = document.createElement('div');
+        posterContainer.classList.add('poster-container');
+        const posterImg = document.createElement('img');
+        posterImg.src = this.poster;
+        const bottomGradient = document.createElement('div');
+        bottomGradient.classList.add('bottom-gradient');
+
+        const titleQuestion = document.createElement('p');
+        // la je lui dis que ce que je veux à la place de mon texte c'est ce que j'ai dans mon objet Question
+        titleQuestion.textContent = this.statement;
+
+        const questionsContainerRef = document.createElement('div');
+        questionsContainerRef.classList.add('questions-container');
+
+        // la c'est le bordel, accroche toi à ton slip// 
+        // On boucle dans mon tableau de liste d'acteur(answerList), à chaque fois que je bouvcle, je creer la partie radio+label+actor
+        for (let i = 0; i < this.answerList.length; i++) {
+
+            const labelRef = document.createElement('label');
+            const inputRef = document.createElement('input');
+            inputRef.type = 'radio';
+            inputRef.name = 'answer' + index;
+
+            const spanRef = document.createElement('span');
+            spanRef.textContent = this.answerList[i];
+
+            labelRef.appendChild(inputRef);
+            labelRef.appendChild(spanRef);
+            questionsContainerRef.appendChild(labelRef)
+        }
+        // en gros on range les boites dans d'autres boites // 
+        posterContainer.appendChild(posterImg);
+        posterContainer.appendChild(bottomGradient);
+        divQuestion.appendChild(posterContainer)
+        divQuestion.appendChild(titleQuestion)
+        divQuestion.appendChild(questionsContainerRef);
+
+        document.getElementById('questionsWrapper').appendChild(divQuestion)
+        // document.body.appendChild(divQuestion);
+    }
+
 }
